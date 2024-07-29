f57d1d9d10562c1a6955400a2970f92a9b612c47
Sergey Fukanchik <s.fukanchik@postgrespro.ru>
2024-07-29 20:36:02 +0300

    12

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int
-main()
+static char*
+get_compression_path(const char *relpath)
 {
-  printf("TEST\n");
+    char pg_compression_path[8192];
+    char szDir1[_MAX_DIR];
+    char szDir[_MAX_DIR];
+    errno_t rc;
+
+    /* Not using 'drive' as relation_path is relative to $PGDATA */
+    rc = _splitpath_s(relpath, NULL, 0, szDir1, _MAX_DIR, NULL, 0, NULL, 0);
+    if (rc)
+    return NULL;
+
+    rc = _splitpath_s(szDir1, NULL, 0, szDir, _MAX_DIR, NULL, 0, NULL, 0);
+    if (rc)
+        return NULL;
+
+    _snprintf(pg_comporession_path, "%s\\xxx", szDir);
+
+    return strdup(pg_compression_path);
+}
+
+int main()
+{
+  char *p = get_compression_path("a/b/c/d");
+  printf("%s\n", p);
   return 0;
 }
